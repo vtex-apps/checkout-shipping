46e6c51aeeff8cae0cc6eafa200edc4090c16064
Jefferson <jefiim.ad@gmail.com>
2020-11-12 17:51:14 -0300

    Show the pickup option details

--- react/ShippingOption.tsx
+++ react/ShippingOption.tsx
@@ -49,6 +49,52 @@ const ShippingOption: React.VFC<Props> = ({
           <TranslateEstimate shippingEstimate={deliveryOption.estimate ?? ''} />
         </span>
         <div>
+        <ButtonPlain size="small" onClick={() => setShowPickupModal(true)}>Ver Detalhes</ButtonPlain>
+        <Modal
+          isOpen={showPickupModal}
+          onClose={() => setShowPickupModal(false)}
+          title={'Detalhes'}
+        >
+          <Divider orientation="horizontal" />
+          <div className="flex-column mb4 mt4">
+            <h2 className="fw5">{getName(deliveryOption)}</h2>
+            <p className="c-on-base">
+              <TranslateEstimate shippingEstimate={deliveryOption.estimate ?? ''} />
+            </p>
+          </div>
+
+          <div className="flex mb4">
+            <div className="flex-column">
+              <span className="fw5 mb2">Endereço</span>
+              <div className="c-on-base">{`${deliveryOption.address?.street}, ${deliveryOption.address?.number}`}</div>
+              <div className="c-on-base">{`${deliveryOption.address?.neighborhood} - ${deliveryOption.address?.city} - ${deliveryOption.address?.state}`}</div>
+            </div>
+          </div>
+
+          <div className="flex-column mb4">
+            <span className="fw5 mb2 mt2">Horário de funcionamento</span>
+            <div className="flex-row justify-around c-on-base mb2 mt2">
+              <span>Segunda a Sexta-feira</span>
+              <span>9h a 21h</span>
+            </div>
+            <Divider orientation="horizontal" />
+            <div className="flex-row c-on-base mb2 mt2">
+              <span>Sábado</span>
+              <span>9h a 21h</span>
+            </div>
+            <Divider orientation="horizontal" />
+            <div className="flex-row c-on-base mb2 mt2">
+              <span>Domingo</span>
+              <span>9h a 15h</span>
+            </div>
+            <Divider orientation="horizontal" />
+          </div>
+
+          <div className="flex-column mb4">
+            <div className="fw5 mb2">Itens disponíveis</div>
+            <div>Todos os itens estão disponíveis nesta loja.</div>
+          </div>
+        </Modal>
         </div>
       </div>
       <div
