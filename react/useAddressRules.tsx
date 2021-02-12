f44e3338b7ac7bc957cf98dbbedeb4bb489c6e02
Lucas Cordeiro <ecdb.lucas@gmail.com>
2021-02-12 14:45:34 -0300

    Use new address rules hook from address-context

--- react/useAddressRules.tsx
+++ react/useAddressRules.tsx
@@ -1,33 +1,3 @@
-import { useMemo } from 'react'
-import { useQuery } from 'react-apollo'
-import { OrderShipping } from 'vtex.order-shipping'
-
-import addressRulesQuery from './graphql/addressRulesQuery.gql'
-
-const { useOrderShipping } = OrderShipping
-
-const useAddressRules = () => {
-  const { countries } = useOrderShipping()
-
-  const { data } = useQuery(addressRulesQuery, { variables: { countries } })
-
-  const addressRules = useMemo(
-    () =>
-      data?.countriesData.reduce(
-        (rules: any, countryData: any) => ({
-          ...rules,
-          [countryData.countryISO]: {
-            display: countryData.display,
-            fields: countryData.addressFields,
-            locationSelect: countryData.locationSelect,
-          },
-        }),
-        {}
-      ),
-    [data]
-  )
-
-  return addressRules
-}
+import { useAddressRules } from 'vtex.address-context'
 
 export default useAddressRules
